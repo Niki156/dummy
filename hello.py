2ec68839c89544b4d4b99c236409fad75eb6d308
Nikhil D <56192574+Niki156@users.noreply.github.com>
2024-11-13 00:31:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,5 +5,6 @@ print ('argument list', sys.argv)
 password = sys.argv[1]
 print ("Hello Nikhil. Authenticating the password...")
 print(f"Entered password: {password}")
-assert password == 'pass', "Incorrect password"
+assert password == 'pass', f"Incorrect password: {password}"
+print("Welcome Boss")
 
